ad4ab8311afc7a56e7833f1fb1db3095ae0c1f27
zarab <musfiqzarab@iut-dhaka.edu>
2019-04-08 13:33:19 +0600

    Creating Search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,20 @@
 console.log("Client side javascript file is loaded");
 
-fetch("http://localhost:3000/weather?address=!").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector("form");
+const searchLocation = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const location = searchLocation.value;
+  //   console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
   });
 });
